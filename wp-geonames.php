8565af762e767edec65e8f2a8f9fb36e58f5efe4
Bhujagendra Ishaya <bhujagendra@thebrightpath.com>
2020-10-06 12:04:24 +0200

    [TASK] Bump Version to 3.1.0

--- wp-geonames.php
+++ wp-geonames.php
@@ -5,7 +5,7 @@ Author: Jacques Malgrange, Bhujagendra Ishaya
 Text Domain: wpGeonames
 Domain Path: /lang
 Description: Allows you to insert all or part of the global GeoNames database in your WordPress base.
-Version: 3.0.1
+Version: 3.1.0
 Author URI: https://www.boiteasite.fr
 */
 
