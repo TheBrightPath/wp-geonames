9f1e113f36efe9ee090c73c4d06658dee6c56341
Bhujagendra Ishaya <bhujagendra@thebrightpath.com>
2020-10-19 11:17:30 +0200

    [TASK] Bump Version to 3.4.0

--- src/Core.php
+++ src/Core.php
@@ -53,7 +53,7 @@ class Core
         ];
 
     // version
-    public const geoVersion = "3.3.0";
+    public const geoVersion = "3.4.0";
 
     // tables constants
     public const tblCountries           = 'wp_geonames_countries';
